d01822ebb46ed4c53008123f3ea4a55dd14d40bf
Tanley Bench (Salt Lake City) <tbench@usanainc.com>
2022-04-18 15:48:56 -0600

    Brainstorm Project Desing

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import sqlite3
+import pickle
+import os
 import Shift
+import threading
 
 if __name__ == '__main__':
     db_name = ''
     conn = sqlite3.connect(db_name)
+    if any(os.scandir("./pickle")):
+        # If pickle directory is not empty, load the pickle object, run the task, pickle object, and go to sleep
+        print("Not Empty")
+    else:
+        # if pickle directory is empty, create the object from the DB, run the task, pickle the object, and go to sleep
+        print("Empty")
+    # Initially, create object from database and marshall object and put tasks to sleep
     # get shift_names from sql
-    # Loop throught the shiftname list and create shift objects, pass the sql connection to the object
+    # Subsequently, unmarshall objects and run the file, remarshall object and put task to sleep
+    # Loop through the shiftname list and create shift objects, pass the sql connection to the object
     # Trigger a continuos task thread until the shifts are all filled
     # Once all shifts are filled, end the thread by sending an email to me
