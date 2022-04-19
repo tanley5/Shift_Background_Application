a71126883d4de9d418862d4e97c9a632ae0fe06c
Tanley Bench (Salt Lake City) <tbench@usanainc.com>
2022-04-19 07:56:38 -0600

    Connected the offline tool to django sqlite3 db

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import Shift
 import threading
 
 if __name__ == '__main__':
-    db_name = ''
+    db_name = 'C:/Users/tbench/Documents/django-projects/shiftbid_stuff/Shift_Seniority_Updated/db.sqlite3'
     conn = sqlite3.connect(db_name)
     if any(os.scandir("./pickle")):
         # If pickle directory is not empty, load the pickle object, run the task, pickle object, and go to sleep
@@ -13,6 +13,15 @@ if __name__ == '__main__':
     else:
         # if pickle directory is empty, create the object from the DB, run the task, pickle the object, and go to sleep
         print("Empty")
+        if (conn):
+            print("Connected to sqlite3")
+
+            cur = conn.cursor()
+            for row in cur.execute('''SELECT report_name FROM shiftbid_shiftbid'''):
+                #print(row[0])
+                shift = Shift.Shift(row[0],conn)
+                print(shift.report_name)
+
     # Initially, create object from database and marshall object and put tasks to sleep
     # get shift_names from sql
     # Subsequently, unmarshall objects and run the file, remarshall object and put task to sleep
